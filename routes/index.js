2bc31a83810a15a6f50951e8fb2e3eb276952c79
Hrant <harvey.mirijanyan@gmail.com>
2017-05-18 20:16:11 -0700

    add cors

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,13 @@ var router = express.Router();
 
 var db = require('../queries');
 
-router.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+router.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
- });
- 
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
