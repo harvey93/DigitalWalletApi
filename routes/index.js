2a47633bc4d04e7d5e7be9eed40c82e66b1b1c99
Hrant <harvey.mirijanyan@gmail.com>
2017-05-18 20:11:39 -0700

    Add access controll headers

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,12 @@ var router = express.Router();
 
 var db = require('../queries');
 
+router.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+ 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
