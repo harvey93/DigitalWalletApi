8d0610a7f8d9279b1c243ea6397d848651da6c5f
Hrant <harvey.mirijanyan@gmail.com>
2017-05-21 11:20:54 -0700

    Add lastname to query

--- queries.js
+++ queries.js
@@ -201,8 +201,8 @@ function getUserPayments(req, res, next) {
 function updatePayment(req, res, next) {
   console.log(req.params.id);
   console.log(req.body);
-  db.query('update payments set user_id=$1, firstname=$2 where id=$3',
-    [parseInt(req.body.user_id), req.body.firstname, parseInt(req.params.id)])
+  db.query('update payments set user_id=$1, firstname=$2, lastname=$3 where id=$4',
+    [parseInt(req.body.user_id), req.body.firstname, req.body.lastname, parseInt(req.params.id)])
     .then(function () {
       res.status(200)
         .json({
